e209bbc67e3b0714c5ead1507d89d380dc62e630
Emilio Rodríguez <emilio.rodriguez.oliman@gmail.com>
2025-10-05 14:03:02 -0600

    chore: redirect /favicon.ico -> /icon.png

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/icon.png', // generado por Next desde app/icon.png
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig;
